8607166be010ea4de87e2efc0d878406bec96f86
Aksachlisimo <simo_akk@hotmail.com>
2024-09-18 18:33:03 +0100

    test

--- server.js
+++ server.js
@@ -93,31 +93,6 @@ app.post('/increment-view/:id', async (req, res) => {
 
 module.exports = router;
 
-// Serve static HTML files
-app.use(express.static(path.join(__dirname, 'public')));
-
-// Serve CSS and JS files
-app.use('/CSS', express.static(path.join(__dirname, 'CSS')));
-app.use('/JS', express.static(path.join(__dirname, 'JS')));
-app.use('/images', express.static(path.join(__dirname, 'images')));
-
-
-// Routes to serve HTML pages
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
-});
-
-app.get('/services', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'services.html'));
-});
-
-app.get('/contact', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'contact.html'));
-});
-
-app.get('/reviews', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'reviews.html'));
-});
 
 // API endpoint to get all services
 app.get('/api/services', async (req, res) => {
