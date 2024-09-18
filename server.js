04cbc83a75efb5b40b38d22156e45809a11f7063
Aksachlisimo <simo_akk@hotmail.com>
2024-09-18 18:38:34 +0100

    changes

--- server.js
+++ server.js
@@ -244,6 +244,6 @@ app.post('/api/feeding-records', async (req, res) => {
 
 
 // Start the server
-app.listen(5000, () => {
-  console.log('Server is running on port 5000');
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
