b49bca7e5f71794914097189de0c73e1caba9e19
Aksachlisimo <simo_akk@hotmail.com>
2024-09-23 18:01:09 +0100

    done

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -1,13 +1,18 @@
 const pool = require('../models/db');
 
 exports.submitContact = async (req, res) => {
-  const { name, email, message } = req.body;
-
-  try {
-    await pool.query('INSERT INTO contacts (name, email, message) VALUES ($1, $2, $3)', [name, email, message]);
-    res.status(200).json({ message: 'Contact submitted successfully' });
-  } catch (err) {
-    console.error("Error in submitting contact:", err);
-    res.status(500).json({ error: err.message });
-  }
-};
+    const { titre, description, email } = req.body;
+  
+    // Log the request body to verify data
+    console.log('Attempting to submit contact...');
+    console.log('Request Body:', req.body);
+  
+    try {
+      await pool.query('INSERT INTO contacts (title, description, email) VALUES ($1, $2, $3)', [title, description, email]);
+      res.status(200).json({ message: 'Contact submitted successfully' });
+    } catch (err) {
+      console.error("Error in submitting contact:", err);
+      res.status(500).json({ error: err.message });
+    }
+  };
+  
