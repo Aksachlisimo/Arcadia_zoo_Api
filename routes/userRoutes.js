66a212f1c4ced3ab045e52a7678a82a08d7a6165
Aksachlisimo <simo_akk@hotmail.com>
2024-09-24 16:57:32 +0100

    test

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const userController = require('../controllers/userController');
 
 // Define routes
-router.post('/login', userController.login);
+router.post('/', userController.login);
 // Add more routes for user management
 
 module.exports = router;
