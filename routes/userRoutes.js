141d57fb0fcfb1e9f4595b687bd5833dcf14a448
Aksachlisimo <simo_akk@hotmail.com>
2024-09-24 17:31:48 +0100

    shi

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const userController = require('../controllers/userController');
 
 // Login route
-router.post('/api/login', userController.login); // Use POST /api/login
-router.post('/api/register', userController.register); 
+router.post('/', userController.login); // Use POST /api/login
+router.post('/', userController.register); 
 
 module.exports = router;
